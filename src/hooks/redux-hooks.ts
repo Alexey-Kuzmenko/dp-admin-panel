155832fd31ae59c230879cccd77f0c49ca1aa36b
Alexey Kuzmenko <alexey.kuzmenko1101@gmail.com>
2024-05-24 11:31:41 +0300

    feat: installed Redux and prepared application store

---
+++ src/hooks/redux-hooks.ts
@@ -0,0 +1,6 @@
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+import { RootState } from '../store';
+import { AppDispatch } from '../store';
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
