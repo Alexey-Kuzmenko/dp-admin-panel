a766da2c4cf83b72908bcad2145b6b9cdb005661
Oleksii Kuzmenko <alexey.kuzmenko1101@gmail.com>
2024-12-18 13:32:36 +0200

    feat: added integration with API in Content slice

--- src/store/contentSlice.ts
+++ src/store/contentSlice.ts
@@ -1,96 +1,179 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { asyncThunkCreator, buildCreateSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios, { AxiosResponse } from 'axios';
+
 import { Content, ContentModel } from '../models/content.model';
-import { v4 as uuidv4 } from 'uuid';
 import { CreateContentDto } from '../dto/content.dto';
+import { ResponseError } from '../types/response-error.type';
+import excludeObjectValues from '../utils/excludeObjectValues';
+import { ERROR_MSG_TEMPLATE } from '../constants/constants';
+import { RootState } from '.';
+
+const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+const JWT_TOKEN = import.meta.env.VITE_JWT_TOKEN;
+
+const createContentSlice = buildCreateSlice({
+    creators: { asyncThunk: asyncThunkCreator }
+});
 
 interface ContentSate {
-    content: Array<ContentModel>
+    content: Array<ContentModel>,
+    loading: boolean | null
+    error: ResponseError
 }
 
-// * state with temporary data. This value exported for only for tests
 export const initialState: ContentSate = {
-    content: [
-        {
-            _id: uuidv4(),
-            type: 'about',
-            eng: {
-                _id: uuidv4(),
-                title: 'About Me',
-                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                image: 'https://api.ok-dev.pp.ua/img_1',
-            },
-            ua: {
-                title: ''
-            }
-        },
-        {
-            _id: uuidv4(),
-            type: 'contacts',
-            eng: {
-                _id: uuidv4(),
-                title: 'Get in touch',
-                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                image: 'https://api.ok-dev.pp.ua/img_2',
-            },
-            ua: {
-                title: ''
-            }
-        },
-        {
-            _id: uuidv4(),
-            type: 'services',
-            eng: {
-                _id: uuidv4(),
-                title: 'Get in touch',
-                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                image: 'https://api.ok-dev.pp.ua/img_2',
-            },
-            ua: {
-                title: ''
-            }
-        }
-    ]
+    content: [],
+    loading: null,
+    error: {
+        exists: null,
+        message: null
+    }
 };
 
-const contentSlice = createSlice({
+const contentSlice = createContentSlice({
     name: 'content',
     initialState,
     selectors: {
-        selectContent: (state) => state.content
+        selectContent: (state) => state.content,
+        selectLoading: (state) => state.loading,
+        selectError: (state) => state.error
     },
     reducers: (create) => ({
-        addPageContent: create.reducer((state, { payload }: PayloadAction<CreateContentDto>) => {
-            const newContent: ContentModel = {
-                _id: uuidv4(),
-                ...payload
-            };
-
-            state.content.push(newContent);
-        }),
-
-        deletePageContent: create.reducer((state, { payload }: PayloadAction<string>) => {
+        deletePageContentLocally: create.reducer((state, { payload }: PayloadAction<string>) => {
             state.content = state.content.filter((c) => c._id !== payload);
         }),
+        fetchPagesContent: create.asyncThunk(async () => {
+            const response: AxiosResponse<ContentModel[]> = await axios.get(`${API_URL}/content`, {
+                headers: {
+                    'Api-key': API_KEY
+                }
+            });
 
-        editContent: create.reducer(
-            (state, { payload }: PayloadAction<{ content: Content, formValue: string }>) => {
-                const [type, lang]: Array<string> = payload.formValue.replace(/\s+/g, '').split('|');
-                const pageContent = state.content.find((c) => c.type === type);
+            return response.data;
+        },
+            {
+                pending: (state) => {
+                    state.loading = true;
+                },
+                fulfilled: (state, { payload }) => {
+                    const data = excludeObjectValues<ContentModel>(['createdAt', 'updatedAt', '__v'], payload);
+                    state.content.push(...data);
+                },
+                rejected: (state, { error }) => {
+                    state.error.exists = true;
+                    state.error.message = error.message ? error.message : `${ERROR_MSG_TEMPLATE} Content slice`;
+                },
+                settled: (state) => {
+                    state.loading = false;
+                }
+            }
+        ),
+        addPageContent: create.asyncThunk(async (dto: CreateContentDto) => {
+            const response: AxiosResponse<ContentModel> = await axios.post(`${API_URL}/content/create`, dto, {
+                headers: {
+                    'Authorization': `Bearer ${JWT_TOKEN}`
+                }
+            });
 
-                if (pageContent) {
-                    const pageContactIndex = state.content.indexOf(pageContent);
-                    const contentCopy = [...state.content];
-                    lang === 'eng' ? pageContent.eng = payload.content : pageContent.ua = payload.content;
-                    contentCopy[pageContactIndex] = pageContent;
-                    state.content = contentCopy;
+            return response.data;
+        },
+            {
+                pending: (state) => {
+                    state.loading = true;
+                },
+                fulfilled: (state, { payload }) => {
+                    const data = excludeObjectValues<ContentModel>(['createdAt', 'updatedAt', '__v'], [payload]);
+                    state.content.push(...data);
+                },
+                rejected: (state, { error }) => {
+                    state.error.exists = true;
+                    state.error.message = error.message ? error.message : `${ERROR_MSG_TEMPLATE} Content slice`;
+                },
+                settled: (state) => {
+                    state.loading = false;
+                }
+            }
+        ),
+        deletePageContent: create.asyncThunk(async (id: string, thunkApi) => {
+            await axios.delete(`${API_URL}/content/${id}`, {
+                headers: {
+                    'Authorization': `Bearer ${JWT_TOKEN}`
+                }
+            });
+
+            thunkApi.dispatch(deletePageContentLocally(id));
+        },
+            {
+                pending: (state) => {
+                    state.loading = true;
+                },
+                rejected: (state, { error }) => {
+                    state.error.exists = true;
+                    state.error.message = error.message ? error.message : `${ERROR_MSG_TEMPLATE} Content slice`;
+                },
+                settled: (state) => {
+                    state.loading = false;
+                }
+            }
+        ),
+        editContent: create.asyncThunk(async (payload: { content: Content, formValue: string }, thunkApi) => {
+            const [type, lang]: Array<string> = payload.formValue.replace(/\s+/g, '').split('|');
+            const state = thunkApi.getState() as RootState;
+            const pageContent = state.content.content.find((c) => c.type === type);
+
+            if (pageContent) {
+                const { _id } = pageContent;
+                const response: AxiosResponse<ContentModel> = await axios.patch(`${API_URL}/content/${_id}/${lang}`,
+                    payload.content,
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${JWT_TOKEN}`
+                        }
+                    }
+                );
+                return { type, lang, data: response.data };
+            }
+        },
+            {
+                pending: (state) => {
+                    state.loading = true;
+                },
+                fulfilled: (state, { payload }) => {
+                    if (payload) {
+                        const pageContent = state.content.find((c) => c.type === payload.type);
+
+                        if (pageContent) {
+                            const pageContentIndex = state.content.indexOf(pageContent);
+                            const contentCopy = [...state.content];
+                            payload.lang === 'eng' ?
+                                pageContent.eng = payload.data.eng : pageContent.ua = payload.data.ua;
+
+                            contentCopy[pageContentIndex] = pageContent;
+                            state.content = contentCopy;
+                        }
+                    }
+                },
+                rejected: (state, { error }) => {
+                    state.error.exists = true;
+                    state.error.message = error.message ? error.message : `${ERROR_MSG_TEMPLATE} Content slice`;
+                },
+                settled: (state) => {
+                    state.loading = false;
                 }
             }
         )
     })
 });
 
-export const { selectContent } = contentSlice.selectors;
+export const { selectContent, selectLoading, selectError } = contentSlice.selectors;
 
-export const { addPageContent, deletePageContent, editContent } = contentSlice.actions;
+export const {
+    deletePageContentLocally,
+    fetchPagesContent,
+    addPageContent,
+    deletePageContent,
+    editContent
+} = contentSlice.actions;
 
 export default contentSlice.reducer;
--- src/utils/generateContentFormValues.ts
+++ src/utils/generateContentFormValues.ts
@@ -3,10 +3,6 @@ import { ContentModel } from '../models/content.model';
 export default function generateContentFormValues(content: Array<ContentModel>): Array<string> {
     const formValues: Array<string> = [];
 
-    if (!content.length) {
-        throw new Error('Array with content values cannot be empty.');
-    }
-
     for (let i = 0; i < content.length; i++) {
         const c = content[i];
         formValues.push(`${c.type} | eng`);
